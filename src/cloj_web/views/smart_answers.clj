0d71222f37b72f91d7ecac91e5aab29963147ed8
Jim Barritt <jim@planet-ix.com>
2012-10-19 15:07:45 +0100

    [jmdb] Added some styling and got a basic section worked out

--- src/cloj_web/views/smart_answers.clj
+++ src/cloj_web/views/smart_answers.clj
@@ -1,9 +1,25 @@
 (ns cloj-web.views.smart-answers
   (:require [cloj-web.views.common :as common])
-  (:use [noir.core :only [defpage]]))
+  (:use noir.core
+        hiccup.core
+        hiccup.page
+        hiccup.form))
 
+
+(defpartial about-you-section [{:keys [firstname lastname]}]
+  [:fieldset#about-you
+   [:legend "About you"]
+   [:p.group
+    (label "firstname" "First name: ")
+    (text-field "firstname" firstname)]
+   [:p.group
+    (label "lastname" "Last name: ")
+    (text-field "lastname" lastname)]])
 
 (defpage "/smart-answer-demo" []
-         (common/layout
-          [:h1 "Please fill out this form"]
-           ))
+  (common/layout
+   [:h1 "Please fill out this form:"]
+   [:div.guide
+    (form-to [:post "/smart-answer-demo" {:class "form-top"}]
+             (about-you-section {})
+             (submit-button "Next->"))]))
