f3f2a0abaaf80f1f5b5568f672335889969d71c1
Jim Barritt <jim@planet-ix.com>
2012-10-19 17:17:13 +0100

    [jmdb] Working out algorithm to parse the keys

--- src/cloj_web/views/guided_answers.clj
+++ src/cloj_web/views/guided_answers.clj
@@ -12,42 +12,80 @@
     (label id label-text)
     (text-field id data)])
 
+(defn section-button [questionId]
+  [:input {:type "submit" :value "Change Answer" :name (format "active-%s" questionId)}])
 
-(defpartial question-1 [{:keys [firstname lastname]}]
+
+(defpartial question-1 [{:keys [firstname lastname]} active?]
   [:fieldset#question-1
    [:legend "Your name"]
+   (if active?
+     (label "active" "I AM ACTIVE !!!!!!!!!"))
    (text-box "firstname", "First name: ", firstname)
-   (text-box "lastname", "Last name: ", lastname)])
+   (text-box "lastname", "Last name: ", lastname)
+   (section-button "1")])
 
-(defpartial question-2 [{:keys [age]}]
+(defpartial question-2 [{:keys [age]} active?]
   [:fieldset#question-1
    [:legend "Your age"]
-   (text-box "age", "Age: ", age)])
+   (if active?
+     (label "active" "I AM ACTIVE !!!!!!!!!"))
+   (text-box "age", "Age: ", age)
+   (:input "Edit")
+   (section-button "2")])
 
 
 (defn calculate-question [applicationForm]
-  (let [ currentQ (:question applicationForm)]
+  (let [currentQ (:question applicationForm)]
        (if (= nil currentQ)
          1
          (+ (Integer/parseInt currentQ) 1))))
 
-;; (def demoForm
-;;   {:question "1"})
+(def demoForm
+  {:question "1"
+   :active-1 "Change+Answer"})
 
-;; (calculate-question demoForm)
-;; (= nil (:question demoForm))
+(defn is-active [formData]
+  )
 
 
-(defpage [:get "/guided-answer-demo"] {:as applicationForm}
+(defn key-starts-with [prefix mapEntry]
+    (.startsWith (str (key mapEntry)) prefix))
+
+(defn str-key [mapEntry]
+  (str (key mapEntry)))
+
+
+(defn active-keys [aMap]
+  (map str-key (filter #(key-starts-with ":active" %1) demoForm)))
+
+(defn question-id-from [keyName]
+  (last (seq (.split keyName "-"))))
+
+
+
+(map question-id-from (active-keys demoForm))
+
+
+
+
+
+(seq (.split "a-1" "-"))
+
+
+
+
+
+(defpage [:get "/guided-answer-demo"] {:as formData}
   (common/layout
    [:h1 "Please fill out this form:"]
    [:div.guide
     (form-to [:post "/guided-answer-demo" {:class "form-top"}]
-             (text-field "question", (calculate-question applicationForm))
-             (question-1 applicationForm)
-             (question-2 applicationForm)
+             (text-field "question", (calculate-question formData))
+             (question-1 formData (is-active? formData "1"))
+             (question-2 formData (is-active? formData "2"))
              (submit-button "Next->"))]))
 
-(defpage [:post "/guided-answer-demo"] {:as applicationForm}
-  (redirect (url "/guided-answer-demo" applicationForm)))
+(defpage [:post "/guided-answer-demo"] {:as formData}
+  (redirect (url "/guided-answer-demo" formData)))
 
