7ec0779bd99b0f202658303681ef093bf02beb00
Jim Barritt <jim@planet-ix.com>
2012-10-19 16:05:03 +0100

    [jmdb] Got simple parameters being passed around

--- src/cloj_web/views/guided_answers.clj
+++ src/cloj_web/views/guided_answers.clj
@@ -1,25 +1,40 @@
 (ns cloj-web.views.guided-answers
   (:require [cloj-web.views.common :as common])
   (:use noir.core
+        [noir.response :only [redirect]]
         hiccup.core
         hiccup.page
-        hiccup.form))
+        hiccup.form
+        hiccup.util))
+
+(defn text-box [id label-text data]
+  [:p.group
+    (label id label-text)
+    (text-field id data)])
 
 
-(defpartial about-you-section [{:keys [firstname lastname]}]
-  [:fieldset#about-you
-   [:legend "About you"]
-   [:p.group
-    (label "firstname" "First name: ")
-    (text-field "firstname" firstname)]
-   [:p.group
-    (label "lastname" "Last name: ")
-    (text-field "lastname" lastname)]])
+(defpartial question-1 [{:keys [firstname lastname]}]
+  [:fieldset#question-1
+   [:legend "Your name"]
+   (text-box "firstname", "First name: ", firstname)
+   (text-box "lastname", "Last name: ", lastname)])
 
-(defpage "/guided-answer-demo" []
+(defpartial question-2 [{:keys [age]}]
+  [:fieldset#question-1
+   [:legend "Your age"]
+   (text-box "age", "Age: ", age)])
+
+
+
+(defpage [:get "/guided-answer-demo"] {:as applicationForm}
   (common/layout
    [:h1 "Please fill out this form:"]
    [:div.guide
     (form-to [:post "/guided-answer-demo" {:class "form-top"}]
-             (about-you-section {})
+             (question-1 applicationForm)
+             (question-2 applicationForm)
              (submit-button "Next->"))]))
+
+(defpage [:post "/guided-answer-demo"] {:as applicationForm}
+  (redirect (url "/guided-answer-demo" applicationForm)))
+
